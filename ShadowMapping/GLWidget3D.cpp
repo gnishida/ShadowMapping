b7abdf0014b332b1b92f85a86a764576544d8e91
gnishida <gnishida@purdue.edu>
2015-06-23 16:00:41 -0400

    update.

--- ShadowMapping/GLWidget3D.cpp
+++ ShadowMapping/GLWidget3D.cpp
@@ -149,9 +149,9 @@ void GLWidget3D::createVAO() {
 	glEnableVertexAttribArray(0);
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), 0);
 	glEnableVertexAttribArray(1);
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, color));
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, normal));
 	glEnableVertexAttribArray(2);
-	glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, normal));
+	glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, color));
 	glEnableVertexAttribArray(3);
 	glVertexAttribPointer(3, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, texCoord));
 
--- ShadowMapping/Vertex.h
+++ ShadowMapping/Vertex.h
@@ -7,16 +7,16 @@
  */
 struct Vertex {
 	glm::vec3 position;
-	glm::vec3 color;
 	glm::vec3 normal;
+	glm::vec3 color;
 	glm::vec3 texCoord;
 
 	Vertex() {}
 
 	Vertex(const glm::vec3& pos, const glm::vec3& c, const glm::vec3& n, const glm::vec3& tex) {
 		position = pos;
-		color = c;
 		normal = n;
+		color = c;
 		texCoord = tex;
 	}
 };
